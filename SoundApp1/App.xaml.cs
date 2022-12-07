b915f2052fe9d2a7a44273e36058a926bd400d5a
coolcoder613eb <96163908+coolcoder613eb@users.noreply.github.com>
2022-12-07 12:24:19 +1100

    update

---
+++ SoundApp1/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SoundApp1;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SoundApp1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace SoundApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		/*
+		count++;
+        count++;
+
+        if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count}/2 times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);*/
+	}
+}
+
